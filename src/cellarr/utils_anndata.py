acbc55f824a0c68255316f75f52c0f711ada57f7
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-11-22 16:26:51 -0800

    EOD

--- src/cellarr/utils_anndata.py
+++ src/cellarr/utils_anndata.py
@@ -17,7 +17,7 @@ def remap_anndata(
     h5ad_or_adata: Union[str, anndata.AnnData],
     feature_set_order: dict,
     var_feature_column: str = "index",
-    layer_matrix_name: str = "counts",
+    layer_matrix_name: Union[str, List[str]] = "counts",
     consolidate_duplicate_gene_func=sum,
 ) -> csr_matrix:
     """Extract and remap the count matrix to the provided feature (gene) set order from the :py:class:`~anndata.AnnData`
@@ -46,6 +46,9 @@ def remap_anndata(
             Layer containing the matrix to add to TileDB.
             Defaults to "counts".
 
+            Alternatively, may provide a list of layers to extract and add
+            to TileDB.
+
         consolidate_duplicate_gene_func:
             Function to consolidate when the AnnData object contains
             multiple rows with the same feature id or gene symbol.
@@ -53,8 +56,12 @@ def remap_anndata(
             Defaults to :py:func:`sum`.
 
     Returns:
-        A ``csr_matrix`` representation of the assay matrix.
+        A dictionary with the key containing the name of the layer
+        and the output a ``csr_matrix`` representation of the assay matrix.
     """
+
+    if isinstance(layer_matrix_name, str):
+        layer_matrix_name = [layer_matrix_name]
 
     if isinstance(h5ad_or_adata, str):
         adata = anndata.read_h5ad(h5ad_or_adata, "r")
@@ -64,56 +71,62 @@ def remap_anndata(
 
         adata = h5ad_or_adata
 
-    omat = adata.layers[layer_matrix_name]
-
     if len(feature_set_order) == 0:
-        return coo_matrix(
-            ([], ([], [])),
-            shape=(adata.shape[0], len(feature_set_order)),
-        ).tocsr()
+        omat = {}
+        for lmn in layer_matrix_name:
+            omat[lmn] = coo_matrix(
+                ([], ([], [])),
+                shape=(adata.shape[0], len(feature_set_order)),
+            ).tocsr()
+        return omat
 
     if var_feature_column == "index":
         osymbols = adata.var.index.tolist()
     else:
         osymbols = adata.var[var_feature_column].tolist()
 
-    mat, symbols = consolidate_duplicate_symbols(
-        omat,
-        feature_ids=osymbols,
-        consolidate_duplicate_gene_func=consolidate_duplicate_gene_func,
-    )
+    omat = {}
+    for lmn in layer_matrix_name:
+        mat, symbols = consolidate_duplicate_symbols(
+            adata.layers[lmn],
+            feature_ids=osymbols,
+            consolidate_duplicate_gene_func=consolidate_duplicate_gene_func,
+        )
 
-    # figure out which indices to keep from the matrix
-    indices_to_keep = [i for i, x in enumerate(symbols) if x in feature_set_order]
-    symbols_to_keep = [symbols[i] for i in indices_to_keep]
+        # figure out which indices to keep from the matrix
+        indices_to_keep = [i for i, x in enumerate(symbols) if x in feature_set_order]
+        symbols_to_keep = [symbols[i] for i in indices_to_keep]
 
-    mat = mat[:, indices_to_keep].copy()
+        mat = mat[:, indices_to_keep].copy()
 
-    if len(indices_to_keep) == 0:
-        return coo_matrix(
-            ([], ([], [])),
+        if len(indices_to_keep) == 0:
+            omat[lmn] = coo_matrix(
+                ([], ([], [])),
+                shape=(adata.shape[0], len(feature_set_order)),
+            ).tocsr()
+            continue
+
+        # figure out mapping from the current indices to the original feature order
+        indices_to_map = []
+        for x in symbols_to_keep:
+            indices_to_map.append(feature_set_order[x])
+
+        if isinstance(mat, np.ndarray):
+            mat_coo = coo_matrix(mat)
+        elif isinstance(mat, (csr_array, csr_matrix)):
+            mat_coo = mat.tocoo()
+        else:
+            raise TypeError(f"Unknown matrix type: {type(mat)}.")
+
+        # remap gene symbols to the new feature order
+        new_col = np.array([indices_to_map[i] for i in mat_coo.col])
+
+        omat[lmn] = coo_matrix(
+            (mat_coo.data, (mat_coo.row, new_col)),
             shape=(adata.shape[0], len(feature_set_order)),
         ).tocsr()
 
-    # figure out mapping from the current indices to the original feature order
-    indices_to_map = []
-    for x in symbols_to_keep:
-        indices_to_map.append(feature_set_order[x])
-
-    if isinstance(mat, np.ndarray):
-        mat_coo = coo_matrix(mat)
-    elif isinstance(mat, (csr_array, csr_matrix)):
-        mat_coo = mat.tocoo()
-    else:
-        raise TypeError(f"Unknown matrix type: {type(mat)}.")
-
-    # remap gene symbols to the new feature order
-    new_col = np.array([indices_to_map[i] for i in mat_coo.col])
-
-    return coo_matrix(
-        (mat_coo.data, (mat_coo.row, new_col)),
-        shape=(adata.shape[0], len(feature_set_order)),
-    ).tocsr()
+    return omat
 
 
 def consolidate_duplicate_symbols(
@@ -142,12 +155,31 @@ def consolidate_duplicate_symbols(
     if len(set(feature_ids)) == len(feature_ids):
         return matrix, feature_ids
 
-    return mopsy.apply(consolidate_duplicate_gene_func, mat=matrix, group=feature_ids, axis=1)
+    return mopsy.apply(
+        consolidate_duplicate_gene_func, mat=matrix, group=feature_ids, axis=1
+    )
+
+
+def _sanitize_frame_with_missing_cols(frame, expected_columns, num_cells):
+    output = frame.copy()
+    if expected_columns is not None and len(expected_columns) > 0:
+        _common = list(set(expected_columns).intersection(frame.columns))
+        output = pd.DataFrame({})
+
+        if len(_common) > 0:
+            output = frame[_common]
+
+        for col in expected_columns:
+            if col not in output.columns:
+                output[col] = ["NA"] * num_cells
+
+    return output
 
 
 def _extract_info(
     h5ad_or_adata: Union[str, anndata.AnnData],
     var_feature_column: str = "index",
+    var_subset_columns: List[str] = None,
     obs_subset_columns: List[str] = None,
 ) -> Tuple[List[str], pd.DataFrame, int]:
     if isinstance(h5ad_or_adata, str):
@@ -163,18 +195,16 @@ def _extract_info(
     else:
         symbols = adata.var[var_feature_column].tolist()
 
-    features = pd.DataFrame({})
-    if obs_subset_columns is not None and len(obs_subset_columns) > 0:
-        _common = list(set(obs_subset_columns).intersection(adata.obs))
+    symbols_df = adata.var.copy().set_index(symbols)
 
-        if len(_common) > 0:
-            features = adata.obs[_common]
+    symbols_df = _sanitize_frame_with_missing_cols(
+        symbols_df, var_subset_columns, adata.shape[1]
+    )
+    features_df = _sanitize_frame_with_missing_cols(
+        adata.obs, obs_subset_columns, adata.shape[0]
+    )
 
-        for col in obs_subset_columns:
-            if col not in features.columns:
-                features[col] = ["NA"] * adata.shape[0]
-
-    return symbols, features, adata.shape[0]
+    return symbols_df, features_df, adata.shape[0]
 
 
 def _wrapper_extract_info(args):
@@ -185,6 +215,7 @@ def _wrapper_extract_info(args):
 def extract_anndata_info(
     h5ad_or_adata: List[Union[str, anndata.AnnData]],
     var_feature_column: str = "index",
+    var_subset_columns: List[str] = None,
     obs_subset_columns: dict = None,
     num_threads: int = 1,
 ):
@@ -197,6 +228,10 @@ def extract_anndata_info(
         var_feature_column:
             Column containing the feature ids (e.g. gene symbols).
             Defaults to "index".
+
+        var_subset_columns:
+            List of var columns to concatenate across all files.
+            Defaults to None and no metadata columns will be extracted.
 
         obs_subset_columns:
             List of obs columns to concatenate across all files.
@@ -211,7 +246,10 @@ def extract_anndata_info(
             Defaults to False.
     """
     with Pool(num_threads) as p:
-        _args = [(file_info, var_feature_column, obs_subset_columns) for file_info in h5ad_or_adata]
+        _args = [
+            (file_info, var_feature_column, var_subset_columns, obs_subset_columns)
+            for file_info in h5ad_or_adata
+        ]
         return p.map(_wrapper_extract_info, _args)
 
 
@@ -270,5 +308,5 @@ def scan_for_cellmetadata(cache) -> List[int]:
         A :py:class:`pandas.Dataframe` containing all cell metadata.
     """
 
-    _cellmeta = pd.concat([x[1] for x in cache])
+    _cellmeta = pd.concat([x[1] for x in cache], ignore_index=True)
     return _cellmeta
